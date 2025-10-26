b1f926fd8b1528a556f8eeb605ec04d02fa3ad23
saachisainath <ss18735@nyu.edu>
2025-10-26 14:55:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import plotly.express as px
 
 st.markdown("<h1 style='color: cornflowerblue;'>Student Success Predictor</h1>", unsafe_allow_html=True)
 st.markdown("<p style='color: lavender;'>Are you going to survive the semester? Our data will tell!</p>", unsafe_allow_html=True)
-st.image("joshua-hoehne-iggWDxHTAUQ-unsplash.jpg", width=700)
+st.image("joshua-hoehne-iggWDxHTAUQ-unsplash.jpg", use_container_width=True)
 
 df = pd.read_csv("StudentPerformanceFactors.csv")
 ## Automated Report 
